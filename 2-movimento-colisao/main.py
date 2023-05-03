56864353f3bd72d3cf8452a640e414a5c1d3f1d6
luanferreira93 <luancarlosjn@gmail.com>
2023-05-03 17:58:35 -0300

    Colisão

--- 2-movimento-colisao/main.py
+++ 2-movimento-colisao/main.py
@@ -1,4 +1,4 @@
-import sys , pygame
+import sys , pygame, random
 
 pygame.init()
 
@@ -10,6 +10,9 @@ x = largura/2 - 32
 
 y = altura/2 - 32
 
+random_x = random.randint(50,600)
+random_y = random.randint(50,400)
+
 tela = pygame.display.set_mode((largura,altura))
 
 pygame.display.set_caption('Joguinho')
@@ -19,7 +22,6 @@ relogio = pygame.time.Clock()
 
 while True:
     relogio.tick(60)
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
@@ -46,6 +48,11 @@ while True:
   
     tela.fill((0,0,0))
 
-    pygame.draw.rect(tela,(255,255,255),(100,100,40,40))
-    pygame.draw.rect(tela,(255,0,0),(x,y,40,40))
+    quadrado_branco = pygame.draw.rect(tela,(255,255,255),(random_x,random_y,40,40))
+    quadrado_vermelho = pygame.draw.rect(tela,(255,0,0),(x,y,40,40))
+    
+    if quadrado_vermelho.colliderect(quadrado_branco):
+        random_x = random.randint(50,600)
+        random_y = random.randint(50,400)
+
     pygame.display.update()
