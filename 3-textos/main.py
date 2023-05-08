8a4c4cc1d44aa0d7502e09c9a36a0a00f8fd3c80
luanferreira93 <luancarlosjn@gmail.com>
2023-05-07 21:41:18 -0300

    Gerando Texto

--- 3-textos/main.py
+++ 3-textos/main.py
@@ -10,10 +10,6 @@ x = largura/2 - 32
 
 y = altura/2 - 32
 
-fonte = pygame.font.SysFont('ariel',40,True,True)
-
-pontos = 0
-
 random_x = random.randint(50,600)
 random_y = random.randint(50,400)
 
@@ -26,11 +22,20 @@ relogio = pygame.time.Clock()
 
 while True:
     relogio.tick(60)
-    mensagem = f'Pontos: {pontos}'
-    texto_formatado = fonte.render(mensagem,True,(255,255,255))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        # if event.type == pygame.KEYDOWN:
+        #     if event.key == pygame.K_d:
+        #         x += 6
+        #     if event.key == pygame.K_a:
+        #         x -= 6
+        #     if event.key == pygame.K_w:
+        #         y -= 6
+        #     if event.key == pygame.K_s:
+        #         y += 6
+            
+
     if pygame.key.get_pressed()[pygame.K_d]:
         x += 6
     if pygame.key.get_pressed()[pygame.K_a]:
@@ -43,13 +48,11 @@ while True:
   
     tela.fill((0,0,0))
 
-    quadrado_branco = pygame.draw.rect(tela,(255,0,0),(random_x,random_y,40,40))
-    quadrado_vermelho = pygame.draw.rect(tela,(0,0,255),(x,y,40,40))
+    quadrado_branco = pygame.draw.rect(tela,(255,255,255),(random_x,random_y,40,40))
+    quadrado_vermelho = pygame.draw.rect(tela,(255,0,0),(x,y,40,40))
     
     if quadrado_vermelho.colliderect(quadrado_branco):
         random_x = random.randint(50,600)
         random_y = random.randint(50,400)
-        pontos = pontos + 1
 
-    tela.blit(texto_formatado,(480,20))
     pygame.display.update()
