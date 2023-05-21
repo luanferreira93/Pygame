7ea915fcb55e27ddc12b348c5d14c3255458f376
luanferreira93 <luancarlosjn@gmail.com>
2023-05-21 17:13:03 -0300

    lógica crescimento da cobra

--- Snake-Game/main.py
+++ Snake-Game/main.py
@@ -19,12 +19,13 @@ relogio = pygame.time.Clock()
 
 lista_cobra = []
 
+comprimento_inicial = 0
 
 def aumenta_cobra(lista_cobra):
     for XeY in lista_cobra:
         x = XeY[0]
         y = XeY[1]
-        pygame.draw.rect(tela,(0,255,0),(x,y,20,20))
+        pygame.draw.rect(tela,(0,255,0),(x,y,25,25))
         
 
 
@@ -47,17 +48,25 @@ while True:
     tela.fill((0,0,0))
 
     moeda = pygame.draw.circle(tela,(255,215,0),(x_moeda,y_moeda),8)
-    cobra = pygame.draw.rect(tela,(0,255,0),(x_cobra,y_cobra,20,20))
+    cobra = pygame.draw.rect(tela,(0,255,0),(x_cobra,y_cobra,25,25))
 
     lista_cabeca = []
     lista_cabeca.append(x_cobra)
     lista_cabeca.append(y_cobra)
     lista_cobra.append(lista_cabeca)
-    print(lista_cobra)
+    
+    if len(lista_cobra) > comprimento_inicial:
+        del lista_cobra[0]
+
+    aumenta_cobra(lista_cobra)
 
 
     if moeda.colliderect(cobra):
-        aumenta_cobra(lista_cobra)
+        x_moeda = random.randint(50,600)
+        y_moeda = random.randint(50,400)
+
+        comprimento_inicial = comprimento_inicial + 1
+
 
     
 
