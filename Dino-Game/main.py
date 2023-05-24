c0c3de706e0c2e954387a380dd0950dd5fa839b8
luanferreira93 <luancarlosjn@gmail.com>
2023-05-24 16:30:04 -0300

    Lógica de colisão do dino com cacto

--- Dino-Game/main.py
+++ Dino-Game/main.py
@@ -37,6 +37,9 @@ class Dino(pygame.sprite.Sprite):
         self.rect.center = (100, ALTURA - 64)
         self.pulo = False
         self.posicao_y_inicial = (ALTURA - 64 - 96 // 2)
+        # Mascara de Colisão
+        self.mask = pygame.mask.from_surface(self.image)
+        self.som_colisao = pygame.mixer.Sound(os.path.join(diretorio_sons,'death_sound.wav'))
 
     def pular(self):
         self.pulo = True   
@@ -61,6 +64,9 @@ class Dino(pygame.sprite.Sprite):
             else:
                 self.rect.y = self.posicao_y_inicial
     
+    def colisao(self):
+        self.som_colisao.play()
+    
 
 class Nuvens(pygame.sprite.Sprite):
     def __init__(self):
@@ -84,14 +90,31 @@ class Chao(pygame.sprite.Sprite):
         self.image = sprite_sheet.subsurface((6*32,0),(32,32))
         self.image = pygame.transform.scale(self.image,(32*2,32*2))
         self.rect = self.image.get_rect()
-        self.rect.y = ALTURA - 64
+        self.rect.y = ALTURA - 64 
         self.rect.x = pos_x * 64
     
     def update(self):
         if self.rect.topright[0] < 0:
             self.rect.x = LARGURA
         self.rect.x -= 5 
-        pass
+        
+
+class Cacto(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = sprite_sheet.subsurface((5*32,0),(32,32))
+        self.image = pygame.transform.scale(self.image,(32*2,32*2))
+        self.rect = self.image.get_rect()
+        self.rect.x = 300
+        self.rect.center = (LARGURA, ALTURA - 64)
+        # Mascara de Colisão
+        self.mask = pygame.mask.from_surface(self.image)
+    
+    def update(self):
+
+        if self.rect.topright[0] < 0:
+            self.rect.x = LARGURA
+        self.rect.x -= 10
 
 
 todas_as_sprites = pygame.sprite.Group()
@@ -106,15 +129,22 @@ for i in range(LARGURA//64+2):
     chao = Chao(i)
     todas_as_sprites.add(chao)
 
+cacto = Cacto()
+todas_as_sprites.add(cacto)
+
+grupo_obstaculos = pygame.sprite.Group()
+grupo_obstaculos.add(cacto)
+
+
 
 #Trocando o ícone da janela do jogo
 game_icon = dino.imagens_dinossauro[0]
 pygame.display.set_icon(game_icon)
 
-print(game_icon)
-
-
 relogio = pygame.time.Clock()
+
+colidiu = False
+
 while True:
     relogio.tick(30)
     tela.fill(BRANCO)
@@ -130,8 +160,17 @@ while True:
                     pass
                 else:
                     dino.pular()
-
+    colisoes = pygame.sprite.spritecollide(dino,grupo_obstaculos,False,pygame.sprite.collide_mask)
+    
     todas_as_sprites.draw(tela)
-    todas_as_sprites.update()
+
+    #Pausando o jogo caso ocorra uma colisão
+    if colisoes and colidiu == False:
+        dino.colisao()
+        colidiu = True
+    if colidiu:
+        pass
+    else:
+        todas_as_sprites.update()
 
     pygame.display.flip()
