5f5a1f161a88b465fbdc413ae78a6a2781b4abf4
luanferreira93 <luancarlosjn@gmail.com>
2023-05-24 00:43:59 -0300

    Dino Game

---
+++ Dino-Game/main.py
@@ -0,0 +1,77 @@
+import pygame
+from pygame.locals import *
+from sys import exit
+import os
+from random import randrange
+
+diretorio_principal = os.path.dirname(__file__)
+diretorio_imagens = os.path.join(diretorio_principal, 'imagens')
+diretorio_sons = os.path.join(diretorio_principal, 'sons')
+
+LARGURA = 640
+ALTURA = 480
+BRANCO = (255,255,255)
+
+tela = pygame.display.set_mode((LARGURA, ALTURA))
+pygame.display.set_caption('Dino Game')
+sprite_sheet = pygame.image.load(os.path.join(diretorio_imagens, 'dinoSpritesheet.png')).convert_alpha()
+
+class Dino(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.imagens_dinossauro = []
+        #Pegando as sprites do Dino
+        for i in range(3):
+            img = sprite_sheet.subsurface((i*32,0), (32,32))
+            img = pygame.transform.scale(img, (32*3, 32*3))
+            self.imagens_dinossauro.append(img)
+        
+        self.index_lista = 0
+        self.image = self.imagens_dinossauro[self.index_lista]
+        self.rect = self.image.get_rect()
+        self.rect.center = (100, ALTURA - 90)
+
+    def update(self):
+        if self.index_lista > 2:
+            self.index_lista = 0
+        self.index_lista += 0.15
+        self.image = self.imagens_dinossauro[int(self.index_lista)]
+
+class Nuvens(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = sprite_sheet.subsurface((7*32,0),(32,32))
+        self.image = pygame.transform.scale(self.image,(32*3,32*3))
+        self.rect = self.image.get_rect()
+        self.rect.y = randrange(50,200,50)#50 inicio 200 final | (de 50 em 50)
+        self.rect.x = LARGURA - randrange(30,300,90)
+    
+    def update(self):
+        #Posição do campo superior direito do retângulo da sprite
+        if self.rect.topright[0] < 0:
+            self.rect.x = LARGURA
+            self.rect.y = randrange(50,200,50)
+        self.rect.x -= 5
+
+
+todas_as_sprites = pygame.sprite.Group()
+dino = Dino()
+todas_as_sprites.add(dino)
+
+for i in range(4):
+    nuvem = Nuvens()
+    todas_as_sprites.add(nuvem)
+
+relogio = pygame.time.Clock()
+while True:
+    relogio.tick(60)
+    tela.fill(BRANCO)
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+
+    todas_as_sprites.draw(tela)
+    todas_as_sprites.update()
+
+    pygame.display.flip()
